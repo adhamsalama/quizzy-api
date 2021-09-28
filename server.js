327f6bb7ac4e7d0aee339d72778ba1aa85f12a08
Adham Salama <adhamsalama@zohomail.com>
2021-09-28 15:12:10 +0200

    Setup the server

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const mongoose  = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express()
 
-const PORT = 3000
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(require('cookie-parser')())
+app.use(require('cors')())
+app.use(require('morgan')('dev'))
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(e => console.log('Server connected to the database'))
 
 app.get('/', (req, res) => {
     res.send('hello, world')
 })
 
-app.listen(PORT, () => console.log('Server running at http://localhost:3000'))
+app.listen(process.env.PORT, () => console.log(`Server running at http://localhost:${process.env.PORT}`))
